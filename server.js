04da0f3e5bee8371b28165f776b4295203a0f73b
Vendelin2up <amy_pamy90@hotmail.com>
2024-05-30 14:28:49 +0200

    about added

--- server.js
+++ server.js
@@ -28,6 +28,18 @@ app.get("/menu", (req, res) => {
   res.json(coffeMenu);
 });
 
+// En about route
+app.get("/about", (req, res) => {
+  const aboutInfo = {
+    company: "Airbean Coffee",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    coffeeProduction:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  };
+  res.json(aboutInfo);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
