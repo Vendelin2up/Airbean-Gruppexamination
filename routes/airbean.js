99ca71c9fd541c2d8bed1b2874445455f22672cc
Mayonesa90 <mayonaas@hotmail.com>
2024-05-30 15:06:32 +0200

    fixed ruoter and started with add to cart function" "

--- routes/airbean.js
+++ routes/airbean.js
@@ -1,11 +1,35 @@
+import express from 'express'
 import { Router } from "express";
 import menu from "../models/coffeeMenu.js";
+import nedb from 'nedb-promise'
 
 const router = Router();
 
+// Homepage
+router.get('/', (req, res) => {
+  res.send('<h1>This is the landing page</h1>')
+})
 // Get menu
 router.get("/menu", (req, res) => {
-  res.json(menu);
+  res.send(menu);
 });
+
+// POST menu
+router.post('/menu', (req, res) => {
+  const orderId = req.body.id;
+  const selectedProduct = menu.filter((product) => product.id === orderId)
+  //Kontroll om man har konto eller vill forsätta som gäst?
+  if(selectedProduct){
+    const cart = new nedb({filename: 'models/cart.db', autoload: true})
+    const productTitle = selectedProduct[0].title
+    const productPrice = selectedProduct[0].price
+    cart.insert(selectedProduct)
+    res.send(`${productTitle} was successfully added to cart`)
+    return
+  } else {
+    res.status(404).send('The requested product could not be found')
+  }
+
+})
 
 export default router;
--- server.js
+++ server.js
@@ -5,23 +5,24 @@
 // npm i nedb-promise
 import express from "express";
 import menu from "./models/coffeeMenu.js";
-// import bodyParser from "body-parser";
-// import router from "./routes/airbean.js";
+import bodyParser from "body-parser";
+import router from "./routes/airbean.js";
 
 const app = express();
 const PORT = 8080;
 
 //Middlewares
 app.use(express.json());
+app.use("/", router);
 
 // Routes
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use("/menu", router);
-app.get("/menu", (req, res) => {
-  const coffeMenu = menu.res.json(menu);
-  console.log(menu);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// app.get("/menu", (req, res) => {
+//   const coffeMenu = menu.res.json(menu);
+//   console.log(menu);
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
