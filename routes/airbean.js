b24d2840ec6a91a575d8d73f7eafa9deb0cc9cb8
Mayonesa90 <mayonaas@hotmail.com>
2024-05-30 23:12:38 +0200

    fixed bugg

--- routes/airbean.js
+++ routes/airbean.js
@@ -4,6 +4,7 @@ import menu from "../models/coffeeMenu.js";
 import nedb from 'nedb-promise'
 
 const router = Router();
+const cart = new nedb({filename: 'models/cart.db', autoload: true})
 
 // Homepage
 router.get('/', (req, res) => {
@@ -15,21 +16,23 @@ router.get("/menu", (req, res) => {
 });
 
 // POST menu
-router.post('/menu', (req, res) => {
-  const orderId = req.body.id;
-  const selectedProduct = menu.filter((product) => product.id === orderId)
-  //Kontroll om man har konto eller vill forsätta som gäst?
-  if(selectedProduct){
-    const cart = new nedb({filename: 'models/cart.db', autoload: true})
-    const productTitle = selectedProduct[0].title
-    const productPrice = selectedProduct[0].price
-    cart.insert(selectedProduct)
-    res.send(`${productTitle} was successfully added to cart`)
-    return
-  } else {
-    res.status(404).send('The requested product could not be found')
-  }
+router.post('/menu', async(req, res) => {
+  try {
+    const orderId = req.body.id;
+    const selectedProduct = menu.find((product) => product.id === orderId)
 
+    if(!selectedProduct){
+      res.status(404).send('The requested product could not be found')
+    } 
+      
+    await cart.insert(selectedProduct)
+    const productTitle = selectedProduct.title
+    const productPrice = selectedProduct.price
+    res.send(`${productTitle} was successfully added to cart`)
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal Server Error')
+  }
 })
 
 export default router;
