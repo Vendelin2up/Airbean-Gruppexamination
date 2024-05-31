fd1b44f2f1b438e08e546e9f5019a719b77a86d2
RockAnnRoll <144233838+RockAnnRoll@users.noreply.github.com>
2024-05-31 09:28:39 +0200

    AnvändarID

--- routes/airbean.js
+++ routes/airbean.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import menu from "../models/coffeeMenu.js";
+import { createUser, getUserById } from "../models/user.js";
+import { validateUserCreation } from "../middlewares/validation.js";
 
 const router = Router();
 
@@ -8,4 +10,42 @@ router.get("/menu", (req, res) => {
   res.json(menu);
 });
 
+// Skapar användaren och returnerar användar-ID
+router.post("/users", validateUserCreation, (req, res) => {
+  const { username } = req.body;
+  createUser(username, (err, user) => {// Skapar användaren
+    if (err) { // Om det uppstår ett fel
+      return res.status(500).json({ error: "Failed to create user" });// Skicka ett felmeddelande
+    }
+    res.status(201).json({ userId: user.userId });// Skicka användar-ID om inget fel uppstår
+  });
+});
+//Middleware-funktionen validateUserCreation används för att validera inkommande data innan användaren skapas.
+//Om användaren skapas framgångsrikt, returnerar den ett svar med användar-ID
+//Annars returnerar den ett felmeddelande.
+
+//Hämtar användaren med det specificerade användar-ID:t
+router.get ("/users/:userId", (req, res) => {
+  const { userId } = req.params;
+  getUserById(userId, (err, user) => {
+    if (err || !user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json(user);
+  });
+})
+
+// // Hämtar användarens beställningar baserat på det specificerade användar-ID:t
+router.get("/users/:userId/orders", (req, res) => {
+  const { userId } = req.params;
+  getUserById(userId, (err, user) => {
+    if (err || !user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json({ orders: user.orders });
+  });
+});
 export default router;
+
+
+
