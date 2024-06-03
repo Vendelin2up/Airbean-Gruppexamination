bf8d3a415c8989ee1081fdf19ae0c501fea81994
Vendelin2up <amy_pamy90@hotmail.com>
2024-06-03 11:20:56 +0200

    fixat lite middlewares, putsat på loginfunktionen

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,20 +1,46 @@
-
+import menu from "../models/coffeeMenu.js";
 const validateUserCreation = (req, res, next) => {
-   //Funktionen validateUserCreation tar tre argument: req (request), res (response) och next.
-    const { username } = req.body;
+  //Funktionen validateUserCreation tar tre argument: req (request), res (response) och next.
+  const { username } = req.body;
   //Destructuring används för att plocka ut username från req.body.
 
-    if (!username || typeof username !== "string" || username.trim() === "") {
-      return res.status(400).json({ error: "Invalid username" });
-    }
+  if (!username || typeof username !== "string" || username.trim() === "") {
+    return res.status(400).json({ error: "Invalid username" });
+  }
 
-    next();
-  };
+  next();
+};
 
-  // Funktionen kontrollerar om username är giltigt (inte tomt, är en sträng, och inte bara mellanslag).
-  // Om username inte är giltigt, skickar den tillbaka ett felmeddelande.
-  // Om username är giltigt, går den vidare till nästa middleware eller funktion med next().
- 
-  
-  export { validateUserCreation };
-  
+// Funktionen kontrollerar om username är giltigt (inte tomt, är en sträng, och inte bara mellanslag).
+// Om username inte är giltigt, skickar den tillbaka ett felmeddelande.
+// Om username är giltigt, går den vidare till nästa middleware eller funktion med next().
+
+export const validateMenu = (req, res, next) => {
+  const isValid = menu.every((item) => {
+    return typeof item.title === "string" && typeof item.price === "number";
+  });
+
+  if (!isValid) {
+    return res.status(400).json({
+      error:
+        "Invalid menu data. Each item must have a title (string) and a price (number).",
+    });
+  }
+  next();
+};
+
+export const validateAboutData = (req, res, next) => {
+  const { company, description, coffeeProduction } = req.body;
+  if (
+    typeof company !== "string" ||
+    typeof description !== "string" ||
+    typeof coffeeProduction !== "string"
+  ) {
+    return res
+      .status(400)
+      .json({ error: "Invalid about data. All fields must be strings." });
+  }
+  next();
+};
+
+export { validateUserCreation };
